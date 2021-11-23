a31107d8325cdb555a5828f49f628006c98c8a39
Zara Gunner <20084586@mail.wit.ie>
2021-11-23 12:51:08 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,7 @@ import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
 import './db';
 import './seedData'
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -12,7 +13,8 @@ const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 app.use('/api/movies', moviesRouter);
-app.use('/api/genres', genresRouter )
+app.use('/api/genres', genresRouter );
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
