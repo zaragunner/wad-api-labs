76d317e1c2debbd44abff18b660aba1cbbfb3cf1
Zara Gunner <20084586@mail.wit.ie>
2021-11-29 11:41:08 +0000

    Topic 10 - Salt and Hash

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -6,12 +6,11 @@ import genres from './genres'
 
 dotenv.config();
 
-// deletes all user documents in collection and inserts test data
 async function loadUsers() {
   console.log('load user Data');
   try {
     await userModel.deleteMany();
-    await userModel.collection.insertMany(users);
+    await users.forEach(user => userModel.create(user));
     console.info(`${users.length} users were successfully stored.`);
   } catch (err) {
     console.error(`failed to Load user Data: ${err}`);
