49464f7fad45385af6d6b1b33b8bc168f9ff2fff
Zara Gunner <20084586@mail.wit.ie>
2021-11-29 12:03:58 +0000

    Topic 10 - Object Referencing

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -3,6 +3,8 @@ import users from './users';
 import dotenv from 'dotenv';
 import genreModel from '../api/genres/genreModel'
 import genres from './genres'
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
 
 dotenv.config();
 
@@ -17,9 +19,6 @@ async function loadUsers() {
   }
 }
 
-if (process.env.SEED_DB) {
-  loadUsers();
-}
 
 async function loadGenres() {
     console.log('load genre Data');
@@ -32,6 +31,22 @@ async function loadGenres() {
     }
   }
   
-  if (process.env.SEED_DB) {
-    loadGenres();
+ 
+
+  // deletes all movies documents in collection and inserts test data
+export async function loadMovies() {
+  console.log('load seed data');
+  console.log(movies.length);
+  try {
+    await movieModel.deleteMany();
+    await movieModel.collection.insertMany(movies);
+    console.info(`${movies.length} Movies were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load movie Data: ${err}`);
   }
+}
+if (process.env.SEED_DB) {
+  loadUsers();
+  loadGenres();//you may not need this line if you skipped the exercises
+  loadMovies();//ADD THIS LINE
+}
